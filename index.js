590b9b2afc2b6a35137256253fd5840d8f5dad5d
luckyantony <luckyantonyofficial@gmail.com>
2025-03-07 02:52:52 +0300

    More about functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
